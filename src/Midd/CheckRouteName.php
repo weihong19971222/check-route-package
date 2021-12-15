3d021ceec49b943e2ec6eba8520f82ddb71bbe6f
weihong19971222 <46885087+weihong19971222@users.noreply.github.com>
2021-12-15 14:03:40 +0800

    Create CheckRouteName.php

---
+++ src/Midd/CheckRouteName.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+class CheckRouteName{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next){
+        $route = Route::getRoutes()->match($request);
+        $currentroute = $route->getName();
+        if(is_null($currentroute)){
+            return $next($request);
+        }
+        $request->attributes->add(['route-name'=>$currentroute]);
+        return $next($request);
+    }
+}
